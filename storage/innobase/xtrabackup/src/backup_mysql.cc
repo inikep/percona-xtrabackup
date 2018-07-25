99e7dbd6279188440ea2c9bc0df431cb5ac9a04f
Sergei Glushchenko <gl.sergei@gmail.com>
2018-07-25 23:37:40 +0700

    PXB-1569: --history does not work when autocommit is disabled
    Fix is to enable session autocommit when connected.

--- storage/innobase/xtrabackup/src/backup_mysql.cc
+++ storage/innobase/xtrabackup/src/backup_mysql.cc
@@ -193,6 +193,8 @@ MYSQL *xb_mysql_connect() {
 
   xb_mysql_query(connection, "SET SESSION wait_timeout=2147483", false, true);
 
+  xb_mysql_query(connection, "SET SESSION autocommit=1", false, true);
+
   return (connection);
 }
 
