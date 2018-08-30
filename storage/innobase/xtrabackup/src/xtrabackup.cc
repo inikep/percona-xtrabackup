fb36ac392f6cb9a184887f373145e74ae70f6edb
Sergei Glushchenko <gl.sergei@gmail.com>
2018-08-30 21:53:57 +0700

    PXB-1634: CREATE TABLE fails with DUPLICATE KEY error
    CREATE TABLE reported DUPLICATE KEY error after a database restore. The
    error was easy to reproduce with release builds of xtrabackup, but not
    with debug builds.
    
    Starting with MySQL 8 InnoDB does not use SELECT MAX(ai_col) to
    initialize auto increment value on server restart. Instead, it relies on
    two sources:
    
    - mysql.dynamic_table_metadata table contents are used after normal
      server shutdown
    - MLOG_TABLE_DYNAMIC_META redo log records are used to update counters
      which haven't been flushed yet
    
    It turned out that debug builds of xtrabackup backed up the data a bit
    slower than release builds and InnoDB had time to flush auto increment
    counters to dynamic_table_metadata table. During prepare, xtrabackup was
    able to restore this table from usual INSERT and UPDATE log records.
    
    In the case when InnoDB had no time to flush auto-increment data,
    dynamic_table_metadata contained outdated counters after prepare, which
    caused DUPLICATE KEY errors.
    
    Fix is to make sure that xtrabackup is flushing auto increment counters
    it has read from the redo log during prepare.

--- storage/innobase/xtrabackup/src/xtrabackup.cc
+++ storage/innobase/xtrabackup/src/xtrabackup.cc
@@ -2197,6 +2197,14 @@ static bool innodb_init(bool init_dd) {
 
   if (init_dd) {
     dict_load_from_spaces_sdi();
+    if (dict_sys->dynamic_metadata == nullptr)
+      dict_sys->dynamic_metadata =
+          dd_table_open_on_name(NULL, NULL, "mysql/innodb_dynamic_metadata",
+                                false, DICT_ERR_IGNORE_NONE);
+    if (dict_persist->table_buffer == nullptr)
+      dict_persist->table_buffer =
+          ut::new_withkey<DDTableBuffer>(UT_NEW_THIS_FILE_PSI_KEY);
+    srv_dict_recover_on_restart();
   }
 
   srv_start_threads();
