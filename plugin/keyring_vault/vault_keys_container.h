d14e8c68d8e7fc449555628ac204fb91cff61f4a
Sergei Glushchenko <gl.sergei@gmail.com>
2018-09-20 04:31:51 +0700

    PXB-1658: Import keyring vault plugin from PS 8
    keyring_vault plugin imported from PS 8

---
+++ plugin/keyring_vault/vault_keys_container.h
@@ -0,0 +1,30 @@
+#ifndef MYSQL_VAULT_KEYS_CONTAINER_H
+#define MYSQL_VAULT_KEYS_CONTAINER_H
+
+#include <boost/core/noncopyable.hpp>
+#include "i_vault_io.h"
+#include "plugin/keyring/common/keys_container.h"
+
+namespace keyring {
+
+class Vault_keys_container final : public Keys_container,
+                                   private boost::noncopyable {
+ public:
+  Vault_keys_container(ILogger *logger_value) noexcept
+      : Keys_container(logger_value) {}
+
+  bool init(IKeyring_io *keyring_io, std::string keyring_storage_url) override;
+  virtual IKey *fetch_key(IKey *key) override;
+  virtual void set_curl_timeout(uint timeout) noexcept {
+    assert(vault_io != NULL);
+    vault_io->set_curl_timeout(timeout);
+  }
+
+ private:
+  virtual bool flush_to_backup() override;
+  IVault_io *vault_io;
+};
+
+}  // namespace keyring
+
+#endif  // MYSQL_VAULT_KEYS_CONTAINER_H
