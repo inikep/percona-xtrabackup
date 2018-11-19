667cbebe93f13186af24a17d46009591bddeb05f
Sergei Glushchenko <gl.sergei@gmail.com>
2018-11-20 04:22:41 +0700

    PXB-1694: ALTER TABLE ... ALGORITHM=COPY causing prepare to fail
    ALTER TABLE t ... ALGORITHM=COPY works as following (suppose t has
    tablespace ID=10):
    
    1. create temporary tablespace tmp1.ibd with ID=11
    2. rename t.ibd to tmp2.ibd
    3. rename tmp1.ibd to t.ibd (t now has ID=11)
    4. delete tmp2.ibd
    
    During the backup xtrabackup starting to copy the redo log first, then
    it copies the tablespace files. When ALTER TABLE is running continuously
    on the same table it may happen, that t.ibd in the backup has larger
    space ID than the one in the redo log on step 3. It means that
    xtrabackup has copied the file from the middle of the sequence and it
    needs to discard existing file.
    
    This patch also fixes crash while applying metadata (such as autoinc
    counters) on removed tables by making dd_table_load_on_dd_obj return
    nullptr for missing tables.

--- storage/innobase/fil/fil0fil.cc
+++ storage/innobase/fil/fil0fil.cc
@@ -11004,7 +11004,7 @@ byte *fil_tablespace_redo_rename(byte *ptr, const byte *end,
     success = fil_tablespace_open_for_recovery(page_id.space());
 
     if (!success) {
-      ib::info() << "Rename failed. Cannot find'" << from_name << "'!";
+      ib::info() << "Rename failed. Cannot find '" << from_name << "'!";
       return (ptr);
     }
 
@@ -11634,6 +11634,16 @@ static bool fil_op_replay_rename(const page_id_t &page_id,
         /* This can happen during truncate. */
         ib::info(ER_IB_MSG_371) << "Tablespace ID mismatch in '" << name << "'";
       }
+      return (err == DB_SUCCESS);
+    }
+
+    if (err == DB_WRONG_FILE_NAME) {
+      df.close();
+      os_file_delete(innodb_data_file_key, df.filepath());
+      bool success = fil_system->erase_path(df.space_id());
+      ut_a(success);
+    } else {
+      df.close();
       return (err == DB_SUCCESS);
     }
   }
