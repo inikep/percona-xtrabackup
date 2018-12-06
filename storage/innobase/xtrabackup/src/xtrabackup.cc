dc09a57b0de05a195f390f81d4031e2cc330b261
Sergei Glushchenko <gl.sergei@gmail.com>
2018-12-06 08:19:18 +0700

    PXB-1721: Issues with partitioned tables
    Call to deserialize_tablespace_ref always returned error, which stopped
    deserialization of partition indexes at first entry. Since xtrabackup
    does not load and use dd::Tablespace objects, it is safe to skip
    resolving of tablespace_ref.
    
    DD object for individual partition was named "schema/table.partition".
    Fix changes naming to "schema/table#P#partition#SP#subpartition" to
    allow lookup partitions by tablespace file name.

--- storage/innobase/xtrabackup/src/xtrabackup.cc
+++ storage/innobase/xtrabackup/src/xtrabackup.cc
@@ -7070,9 +7070,7 @@ skip_check:
 
       mutex_enter(&(dict_sys->mutex));
       if (!table) {
-        msg("xtrabackup: error: "
-            "cannot find dictionary "
-            "record of table %s\n",
+        msg("xtrabackup: error: cannot find dictionary record of table %s\n",
             space->name);
         goto next_node;
       }
