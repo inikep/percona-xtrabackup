b270c65a714a9a96379abda75db2e2049929057b
Sergei Glushchenko <gl.sergei@gmail.com>
2019-04-08 19:03:36 +0700

    PXB-1813: S3 support in xbcloud
    New features implemented, see the JIRA issue for description.
    Major refactoring done.
    
    ----------------------------------------------------------------------------------
    
    PXB-2165 xbcloud cannot store backups using s3 access key parameters if AWS access key env variables are set (#874)
    
    Problem:
    --------
    If AWS prefix environment variables was set, then xbcloud didn't pick the
    other environment variables. for example, If AWS_ACCESS_KEY_ID is set by
    some application it did not pick set by ACCESS_KEY which would endup
    using wrong value for s3-access-key and unable to store the backup in
    cloud
    
    Analysis:
    --------
    These AWS prefix credentials are commonly used by other applications as well.
    So If a user had set ACCESS_KEY_ID and another application had set
    AWS_ACCESS_KEY_ID then xbcloud was pick AWS_ACCESS_KEY_ID and end up
    using wrong credentials.
    PXB was giving  preference to AWS prefix over other environment variables.
    
    Fix:
    ---
    Introduced new environment variables Prefix with S3 having high preference than AWS*
    S3_ACCESS_KEY_ID,
    S3_SECRET_ACCESS_KEY
    S3_SESSION_TOKEN
    S3_DEFAULT_REGION
    S3_CA_BUNDLE
    S3_ENDPOINT
    like S3_ACCESS_KEY_ID preference would be above AWS_ACCESS_KEY_ID and
    ACCESS_KEY_ID.So users can set S3 prefix variables and xbcloud would pick them
    instead of the variable prefix with AWS.
    
    order of preference in a simple way higher(1) to lower (4)
    1) s3 command line like --s3-access-key
    2) S3 ENV
    3) AWS ENV
    4) ACCESS_KEY*
    
    ----------------------------------------------------------------------------------
    
    PXB-2445 Initializing the libgcrypt in xbcloud
    
    https://jira.percona.com/browse/PXB-2445
    
    Problem:
    When using xbcloud to upload or download backups, the following warning messages were being displayed in /var/log/syslog
    xbcloud: Libgcrypt warning: missing initialization
    
    Analysis
    lbgcrypt is used without initializing in xbcloud/hash.h
    
    Fix:
    use xb_libgcrypt_init to initialize the libgcrypt library
    
    -------------------------------------------------------------------------
    
    Fixed PXB-2711 - Libgcrypt initialization warnings for xtrabackup
    
    https://jira.percona.com/browse/PXB-2711
    
    Libgcrypt initialization warnings for xtrabackup
    
    Problem:
    Version of libgcrypt used on centos7 requires proper Libgcrypt
    initialization, otherwise warnings will be issued at syslog.
    We currently use centos7 for tarballs and relink linked libraries to
    copies we provide unde lib/private.
    This causes the warning to appear on centos7 and all OS's using the
    tarball.
    
    Fix:
    Properly initiate libgcrypt before derive keys.

--- storage/innobase/xtrabackup/src/keyring_plugins.cc
+++ storage/innobase/xtrabackup/src/keyring_plugins.cc
@@ -40,6 +40,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #include "keyring_plugins.h"
 #include "rpl_log_encryption.h"
 #include "xb0xb.h"
+#include "xbcrypt_common.h"
 #include "xtrabackup.h"
 
 #include <map>
@@ -534,6 +535,7 @@ bool xb_tablespace_keys_load_one(const char *dir, const char *transition_key,
     transition_key_len = Encryption::KEY_LEN;
   }
 
+  xb_libgcrypt_init();
   ret = xb_derive_key(transition_key, transition_key_len, salt, sizeof(salt),
                       sizeof(derived_key), derived_key);
 
@@ -684,6 +686,7 @@ bool xb_tablespace_keys_dump(ds_ctxt_t *ds_ctxt, const char *transition_key,
     transition_key_len = Encryption::KEY_LEN;
   }
 
+  xb_libgcrypt_init();
   bool ret = xb_derive_key(transition_key, transition_key_len, salt,
                            sizeof(salt), sizeof(derived_key), derived_key);
 
