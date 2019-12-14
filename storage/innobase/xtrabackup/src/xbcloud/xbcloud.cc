f2fe47d623262b9335fbd88362a21888582b2313
Rahul Malik <contact.rmalik@gmail.com>
2019-12-14 16:34:31 +0530

    PXB-1978 xbcloud fails to obtain data from S3 bucket
        Problem
        -----
    
        xbcloud fail to probe s3 bucket probe-bucket.
    
        Analysis
        ----
    
        xbcloud is using a hardcoded bucket probe-bucket in s3
        which it not able to access.
    
        Fix
        ---
        For probe use bucket provided by user through option s3-bucket

--- storage/innobase/xtrabackup/src/xbcloud/xbcloud.cc
+++ storage/innobase/xtrabackup/src/xbcloud/xbcloud.cc
@@ -18,11 +18,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 *******************************************************/
 
+#include <my_alloc.h>
 #include <my_default.h>
 #include <my_dir.h>
 #include <my_getopt.h>
 #include <my_sys.h>
-#include <my_alloc.h>
 #include <mysql/service_mysql_alloc.h>
 #include <signal.h>
 #include <typelib.h>
@@ -32,6 +32,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #include <set>
 #include <string>
 #include <unordered_map>
+#include "template_utils.h"
 
 #include <curl/curl.h>
 
@@ -1048,7 +1049,7 @@ int main(int argc, char **argv) {
     container_name = opt_s3_bucket;
 
     if (!reinterpret_cast<S3_object_store *>(object_store.get())
-             ->probe_api_version_and_lookup()) {
+             ->probe_api_version_and_lookup(container_name)) {
       return EXIT_FAILURE;
     }
   } else if (opt_storage == GOOGLE) {
@@ -1075,7 +1076,7 @@ int main(int argc, char **argv) {
     container_name = opt_google_bucket;
 
     if (!reinterpret_cast<S3_object_store *>(object_store.get())
-             ->probe_api_version_and_lookup()) {
+             ->probe_api_version_and_lookup(container_name)) {
       return EXIT_FAILURE;
     }
   }
