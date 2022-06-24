b4c9c54bfb527850d59eb48160f6e7c78b3b7713
ikarishinjieva <tac.nil@outlook.com>
2022-06-24 12:06:18 +0000

    Fixed PXB-2810 (Panic when AIO initialization failed)
    https://jira.percona.com/browse/PXB-2810
    
    Problem:
    xb_fil_io_init does not return an error when os_aio_init fail to
    initialize causing the backup to crash.
    
    Fix:
    Adjust xb_fil_io_init and function depending on it to properly handle
    initialization errors.
    
    Thanks to Yan Huang for the patch.

--- storage/innobase/xtrabackup/src/xtrabackup.cc
+++ storage/innobase/xtrabackup/src/xtrabackup.cc
@@ -3527,15 +3527,20 @@ static void xtrabackup_destroy_datasinks(void) {
 
 /************************************************************************
 Initializes the I/O and tablespace cache subsystems. */
-static void xb_fil_io_init(void)
+static bool xb_fil_io_init(void)
 /*================*/
 {
 
-  os_aio_init(srv_n_read_io_threads, srv_n_write_io_threads);
+  if (!os_aio_init(srv_n_read_io_threads, srv_n_write_io_threads)) {
+    xb::error() << "Cannot initialize AIO sub-system.";
+    return false;
+  }
 
   fil_init(LONG_MAX);
 
   fsp_init();
+
+  return true;
 }
 
 /****************************************************************************
@@ -3607,7 +3612,11 @@ ulint xb_data_files_init(void)
 /*====================*/
 {
   os_create_block_cache();
-  xb_fil_io_init();
+
+  if (!xb_fil_io_init()) {
+    return DB_IO_ERROR;
+  }
+
   undo_spaces_init();
 
   return (xb_load_tablespaces());
@@ -4234,7 +4243,9 @@ void xtrabackup_backup_func(void) {
 
   os_create_block_cache();
 
-  xb_fil_io_init();
+  if (!xb_fil_io_init()) {
+    exit(EXIT_FAILURE);
+  }
 
   Redo_Log_Data_Manager redo_mgr;
 
