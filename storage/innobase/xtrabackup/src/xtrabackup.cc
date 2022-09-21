973b330c14684f03bce0c8951e8c65c52c021551
Marcelo Altmann <marcelo.altmann@percona.com>
2022-09-21 08:58:43 -0300

    Implemented PXB-2866 Make PXB 8.0.30 be able to backup 8.0.29 (and prior) server
    https://jira.percona.com/browse/PXB-2866
    
     - Adjusted redo log logic to populate start and end lsn of old redo log
    layout.
     - Changed redo_log.cc to use log_sys->m_files_ctx instead of creating a
    new Log_files_context each time. log_sys->m_files_ctx will already have
    the correct Log_files_ruleset (CURRENT or PRE_8_0_30) that can be used
    to identify if we are dealing with ib_logfile or #ib_redo layout.
     - Adjusted lsn calculation on prepare when checkpoint fall into last
    block. PRE 8.0.30 were writting LOG_BLOCK_CHECKPOINT_NO on 8th byte of
    block header, while on 8.0.30 we write LOG_BLOCK_EPOCH_NO. In cases
    where we have two checkpoints faling in the same file (we dont re write
    the header) when we reach to the next checkpoint and it falls into a new
    block, LOG_BLOCK_CHECKPOINT_NO(which is stored as
    recv_sys->scanned_epoch_no) will jump by two. In this case,
    log_block_epoch_no_is_valid will identify the gap and will consider the
    block as finished, without parsin it.
    
     - Adjusted consumer code to stop advancing after log_Status

--- storage/innobase/xtrabackup/src/xtrabackup.cc
+++ storage/innobase/xtrabackup/src/xtrabackup.cc
@@ -202,6 +202,7 @@ char *xtrabackup_tables_exclude = NULL;
 lsn_t xtrabackup_start_checkpoint;
 
 bool xtrabackup_register_redo_log_consumer = false;
+std::atomic<bool> redo_log_consumer_can_advance = false;
 
 typedef std::list<xb_regex_t> regex_list_t;
 static regex_list_t regex_include_list;
@@ -510,7 +511,7 @@ extern TYPELIB innodb_flush_method_typelib;
 #include "sslopt-vars.h"
 
 bool redo_catchup_completed = false;
-Log_format original_log_format;
+Log_format xtrabackup_original_log_format;
 extern struct rand_struct sql_rand;
 extern mysql_mutex_t LOCK_sql_rand;
 bool xb_generated_redo = false;
@@ -5104,6 +5105,13 @@ static bool xtrabackup_init_temp_log(void) {
   }
 
   log_format = (Log_format)mach_read_from_4(log_buf + LOG_HEADER_FORMAT);
+  xtrabackup_original_log_format = log_format;
+  if (log_format == Log_format::VERSION_8_0_28 ||
+      log_format == Log_format::VERSION_8_0_19) {
+    // We interpret v4 & v5 as v6 by writting proper start lsn header later in
+    // this function
+    log_format = Log_format::VERSION_8_0_30;
+  }
   log_detected_format = log_format;
 
   if (ut_memcmp(log_buf + LOG_HEADER_CREATOR, (byte *)LOG_HEADER_CREATOR_PXB,
@@ -6179,7 +6187,8 @@ static bool xtrabackup_replace_log_file(const char *redo_path,
   memset(log_buf + LOG_HEADER_CREATOR, ' ', 4);
 
   /* restore original log format */
-  mach_write_to_4(log_buf + LOG_HEADER_FORMAT, to_int(original_log_format));
+  mach_write_to_4(log_buf + LOG_HEADER_FORMAT,
+                  to_int(xtrabackup_original_log_format));
   success = os_file_write(write_request, src_path, src_file, log_buf, 0,
                           LOG_FILE_HDR_SIZE);
   if (!success) {
