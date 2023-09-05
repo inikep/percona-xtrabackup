percona/percona-server

PS-5364 feature: Update Hashicorp Vault plugin to use v2 API (Initial)
https://jira.percona.com/browse/PS-5364

Initial implementation from Robert.

plugin/keyring_vault/vault_credentials_parser.h
@@ -6,6 +6,7 @@
 #include <set>
 #include "vault_credentials.h"
 #include "logger.h"
+#include <boost/optional.hpp>
 
 namespace keyring {
 class Vault_credentials_parser {
@@ -17,22 +18,84 @@ class Vault_credentials_parser {
         std::make_pair("secret_mount_point", ""));
     vault_credentials_in_progress.insert(std::make_pair("vault_ca", ""));
     vault_credentials_in_progress.insert(std::make_pair("token", ""));
+    vault_credentials_in_progress.insert(
+        std::make_pair("secret_mount_point_version", ""));
 
-    optional_value.insert("vault_ca");
+    value_options.insert(
+        std::make_pair("vault_url", new Value_options(false)));
+    value_options.insert(
+        std::make_pair("secret_mount_point", new Value_options(false)));
+    value_options.insert(std::make_pair("vault_ca", new Value_options(true)));
+    value_options.insert(std::make_pair("token", new Value_options(false)));
+    value_options.insert(std::make_pair(
+        "secret_mount_point_version",
+        new Value_int_options(true, boost::make_optional<Secure_string>("1"),
+                              boost::make_optional(1),
+                              boost::make_optional(2))));
+  }
+
+  ~Vault_credentials_parser()
+  {
+    for (Value_options_map::iterator iter= value_options.begin();
+         iter != value_options.end(); ++iter)
+      delete iter->second;
   }
 
   bool parse(const std::string &file_url,
              Vault_credentials *vault_credentials);
 
  private:
-  void reset_vault_credentials(Vault_credentials *vault_credentials);
+  class Value_options {
+   public:
+    Value_options(bool                           is_optional,
+                  boost::optional<Secure_string> default_value= boost::none)
+        : is_optional(is_optional), default_value(default_value)
+    {
+    }
+    virtual ~Value_options() {}
+
+    bool                           is_optional;
+    boost::optional<Secure_string> default_value;
+
+    virtual bool process_value_options(const Secure_string &option_name,
+                                       Secure_string &value, ILogger *logger);
+  };
+
+  class Value_int_options : public Value_options {
+   public:
+    Value_int_options(bool                           is_optional,
+                      boost::optional<Secure_string> default_value,
+                      boost::optional<int>           min_value,
+                      boost::optional<int>           max_value)
+        : Value_options(is_optional, default_value),
+          min_value(min_value),
+          max_value(max_value)
+    {
+    }
+
+    virtual ~Value_int_options() {}
+
+    virtual bool process_value_options(const Secure_string &option_name,
+                                       Secure_string &value, ILogger *logger);
+
+    boost::optional<int> min_value;
+    boost::optional<int> max_value;
+  };
+
+  void reset_vault_credentials(Vault_credentials::Map *vault_credentials);
 
   bool parse_line(uint line_number, const Secure_string &line,
-                  Vault_credentials *vault_credentials);
+                  Vault_credentials::Map *vault_credentials_map);
 
-  bool                    is_valid_option(const Secure_string &option) const;
-  Vault_credentials       vault_credentials_in_progress;
-  std::set<Secure_string> optional_value;
+  bool is_valid_option(const Secure_string &option) const;
+
+  bool check_value_constrains(const Secure_string &option,
+                              Secure_string &      value);
+
+  Vault_credentials::Map vault_credentials_in_progress;
+
+  typedef std::map<Secure_string, Value_options *> Value_options_map;
+  Value_options_map                                value_options;
 
   ILogger *           logger;
   static const size_t max_file_size;

plugin/keyring_vault/vault_parser.cc
@@ -95,17 +95,18 @@ bool Vault_parser::retrieve_value_from_map(const Secure_string &map,
 {
   size_t key_tag_pos= Secure_string::npos,
          value_start_pos= Secure_string::npos,
-         value_end_pos= Secure_string::npos;
-  bool         was_error= false;
-  const size_t start_tag_length= strlen(":\"");
+         value_end_pos= Secure_string::npos,
+         start_tag_pos= Secure_string::npos;
+  bool was_error= false;
 
   if ((key_tag_pos= map.find(key)) != Secure_string::npos &&
-      (value_start_pos= map.find(":\"", key_tag_pos)) !=
+      (start_tag_pos= map.find(':', key_tag_pos)) != Secure_string::npos &&
+      (value_start_pos= map.find("\"", start_tag_pos)) !=
           Secure_string::npos &&
-      (value_end_pos= map.find("\"", value_start_pos + start_tag_length)) !=
+      (value_end_pos= map.find("\"", value_start_pos + 1)) !=
           Secure_string::npos)
   {
-    value_start_pos+= start_tag_length;
+    ++value_start_pos;  // skip starting "
     DBUG_ASSERT(value_end_pos > 0);
     value_end_pos--;  // due to closing "
     *value=
@@ -122,6 +123,68 @@ bool Vault_parser::retrieve_value_from_map(const Secure_string &map,
     return true;
   }
   return false;
+}
+
+bool Vault_parser::is_null_tag(const Secure_string &tag) const
+{
+  size_t tag_start_pos= tag.find_first_not_of(" ");
+  return tag.find("null", tag_start_pos) == 0;
+}
+
+bool Vault_parser::is_empty_map(const Secure_string &map) const
+{
+  size_t map_start_pos= map.find_first_not_of(" ");
+  return map.find("{}", map_start_pos) == 0;
+}
+
+bool Vault_parser::get_vault_version(
+    const Vault_credentials &vault_credentials,
+    const Secure_string &mount_points_payload, int &vault_version)
+{
+  Secure_string raw_secret_mount_point(
+      vault_credentials.get_raw_secret_mount_point());
+  size_t secret_mount_point_pos(
+      mount_points_payload.find(raw_secret_mount_point + '/'));
+
+  if (raw_secret_mount_point.empty() ||
+      secret_mount_point_pos == Secure_string::npos)
+    return true;
+
+  Secure_string secret_mount_point_payload(mount_points_payload.substr(
+      secret_mount_point_pos,
+      mount_points_payload.length() - secret_mount_point_pos));
+  DBUG_ASSERT(secret_mount_point_payload.length() > 0);
+
+  static std::string options_tag("\"options\"");
+  size_t options_pos(secret_mount_point_payload.find(options_tag.c_str()));
+  if (options_pos == std::string::npos)
+  {
+    vault_version= 1;  // no "options" sections means we are using version 1
+    return false;
+  }
+
+  size_t options_value_start= secret_mount_point_payload.find_first_not_of(
+      ": ", options_pos + options_tag.length());
+
+  Secure_string options(secret_mount_point_payload.substr(
+      options_value_start,
+      secret_mount_point_payload.length() - options_value_start));
+  //(options_pos + options_tag.length())));
+
+  if (is_null_tag(options) || is_empty_map(options))
+  {
+    vault_version=
+        1;  // version == null or empty map means we are using version 1
+    return false;
+  }
+
+  Secure_string value;
+  if (retrieve_value_from_map(options, "version", &value))
+    return true;
+
+  vault_version= atoi(value.c_str());
+
+  return vault_version <= 0 || vault_version > 2;
 }
 
 bool Vault_parser::parse_errors(const Secure_string &payload,

inikep/percona-xtrabackup

(cherry picked from commit 74987891fd33f7daedc50c76da6d013ebd66df9d)

plugin/keyring_vault/vault_credentials_parser.h
@@ -1,6 +1,7 @@
 #ifndef MYSQL_VAULT_CREDENTIALS_PARSER_H
 #define MYSQL_VAULT_CREDENTIALS_PARSER_H
 
+#include <boost/optional.hpp>
 #include <set>
 #include <string>
 #include "plugin/keyring/common/logger.h"
@@ -15,21 +16,78 @@ class Vault_credentials_parser final {
         std::make_pair("secret_mount_point", ""));
     vault_credentials_in_progress.emplace(std::make_pair("vault_ca", ""));
     vault_credentials_in_progress.emplace(std::make_pair("token", ""));
+    vault_credentials_in_progress.emplace(
+        std::make_pair("secret_mount_point_version", ""));
 
-    optional_value.emplace("vault_ca");
+    value_options.emplace(
+        std::make_pair("vault_url", new Value_options(false)));
+    value_options.emplace(
+        std::make_pair("secret_mount_point", new Value_options(false)));
+    value_options.emplace(std::make_pair("vault_ca", new Value_options(true)));
+    value_options.emplace(std::make_pair("token", new Value_options(false)));
+    value_options.emplace(std::make_pair(
+        "secret_mount_point_version",
+        new Value_int_options(true, boost::make_optional<Secure_string>("1"),
+                              boost::make_optional(1),
+                              boost::make_optional(2))));
+  }
+
+  ~Vault_credentials_parser() {
+    for (auto &options_pair : value_options) delete options_pair.second;
   }
 
   bool parse(const std::string &file_url, Vault_credentials *vault_credentials);
 
  private:
-  void reset_vault_credentials(Vault_credentials *vault_credentials) noexcept;
+  class Value_options {
+   public:
+    explicit Value_options(
+        bool is_optional,
+        boost::optional<Secure_string> default_value = boost::none)
+        : is_optional(is_optional), default_value(std::move(default_value)) {}
+    virtual ~Value_options() = default;
+
+    bool is_optional;
+    boost::optional<Secure_string> default_value;
+
+    virtual bool process_value_options(const Secure_string &option_name,
+                                       Secure_string &value, ILogger *logger);
+  };
+
+  class Value_int_options : public Value_options {
+   public:
+    Value_int_options(bool is_optional,
+                      boost::optional<Secure_string> default_value,
+                      boost::optional<int> min_value,
+                      boost::optional<int> max_value)
+        : Value_options(is_optional, std::move(default_value)),
+          min_value(min_value),
+          max_value(max_value) {}
+
+    ~Value_int_options() override = default;
+
+    bool process_value_options(const Secure_string &option_name,
+                               Secure_string &value, ILogger *logger) override;
+
+    boost::optional<int> min_value;
+    boost::optional<int> max_value;
+  };
+
+  static void reset_vault_credentials(
+      Vault_credentials::Map *vault_credentials) noexcept;
 
   bool parse_line(uint line_number, const Secure_string &line,
-                  Vault_credentials *vault_credentials);
+                  Vault_credentials::Map *vault_credentials_map);
 
   bool is_valid_option(const Secure_string &option) const noexcept;
-  Vault_credentials vault_credentials_in_progress;
-  std::set<Secure_string> optional_value;
+
+  bool check_value_constrains(const Secure_string &option,
+                              Secure_string &value);
+
+  Vault_credentials::Map vault_credentials_in_progress;
+
+  typedef std::map<Secure_string, Value_options *> Value_options_map;
+  Value_options_map value_options;
 
   ILogger *logger;
   static const size_t max_file_size;

plugin/keyring_vault/vault_parser.cc
@@ -85,16 +85,18 @@ bool Vault_parser::retrieve_value_from_map(const Secure_string &map,
                                            Secure_string *value) {
   size_t key_tag_pos = Secure_string::npos,
          value_start_pos = Secure_string::npos,
-         value_end_pos = Secure_string::npos;
+         value_end_pos = Secure_string::npos,
+         start_tag_pos = Secure_string::npos;
   bool was_error = false;
-  const size_t start_tag_length = strlen(":\"");
 
   if ((key_tag_pos = map.find(key)) != Secure_string::npos &&
-      (value_start_pos = map.find(":\"", key_tag_pos)) != Secure_string::npos &&
-      (value_end_pos = map.find("\"", value_start_pos + start_tag_length)) !=
+      (start_tag_pos = map.find(':', key_tag_pos)) != Secure_string::npos &&
+      (value_start_pos = map.find("\"", start_tag_pos)) !=
+          Secure_string::npos &&
+      (value_end_pos = map.find("\"", value_start_pos + 1)) !=
           Secure_string::npos) {
-    value_start_pos += start_tag_length;
-    assert(value_end_pos > 0);
+    ++value_start_pos;  // skip starting "
+    DBUG_ASSERT(value_end_pos > 0);
     value_end_pos--;  // due to closing "
     *value = map.substr(value_start_pos, (value_end_pos - value_start_pos + 1));
   } else
@@ -107,6 +109,62 @@ bool Vault_parser::retrieve_value_from_map(const Secure_string &map,
     return true;
   }
   return false;
+}
+
+bool Vault_parser::is_null_tag(const Secure_string &tag) const {
+  size_t tag_start_pos = tag.find_first_not_of(" ");
+  return tag.find("null", tag_start_pos) == 0;
+}
+
+bool Vault_parser::is_empty_map(const Secure_string &map) const {
+  size_t map_start_pos = map.find_first_not_of(" ");
+  return map.find("{}", map_start_pos) == 0;
+}
+
+bool Vault_parser::get_vault_version(const Vault_credentials &vault_credentials,
+                                     const Secure_string &mount_points_payload,
+                                     int &vault_version) {
+  Secure_string raw_secret_mount_point(
+      vault_credentials.get_raw_secret_mount_point());
+  size_t secret_mount_point_pos(
+      mount_points_payload.find(raw_secret_mount_point + '/'));
+
+  if (raw_secret_mount_point.empty() ||
+      secret_mount_point_pos == Secure_string::npos)
+    return true;
+
+  Secure_string secret_mount_point_payload(mount_points_payload.substr(
+      secret_mount_point_pos,
+      mount_points_payload.length() - secret_mount_point_pos));
+  DBUG_ASSERT(secret_mount_point_payload.length() > 0);
+
+  static std::string options_tag("\"options\"");
+  size_t options_pos(secret_mount_point_payload.find(options_tag.c_str()));
+  if (options_pos == std::string::npos) {
+    vault_version = 1;  // no "options" sections means we are using version 1
+    return false;
+  }
+
+  size_t options_value_start = secret_mount_point_payload.find_first_not_of(
+      ": ", options_pos + options_tag.length());
+
+  Secure_string options(secret_mount_point_payload.substr(
+      options_value_start,
+      secret_mount_point_payload.length() - options_value_start));
+  //(options_pos + options_tag.length())));
+
+  if (is_null_tag(options) || is_empty_map(options)) {
+    vault_version =
+        1;  // version == null or empty map means we are using version 1
+    return false;
+  }
+
+  Secure_string value;
+  if (retrieve_value_from_map(options, "version", &value)) return true;
+
+  vault_version = atoi(value.c_str());
+
+  return vault_version <= 0 || vault_version > 2;
 }
 
 bool Vault_parser::parse_errors(const Secure_string &payload,