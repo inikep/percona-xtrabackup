inikep/percona-server

PS-5364 feature: Update Hashicorp Vault plugin to use v2 API
https://jira.percona.com/browse/PS-5364

Initial implementation from Robert.

PS-5364 feature: Update Hashicorp Vault plugin to use v2 API (MTR combinations)

https://jira.percona.com/browse/PS-5364

Reworked 'keyring_vault' MTR test cases.

Introduced combinations to test different flavors of Vault Server
configurations -'(mount|directory)-(v1|v1|auto_v1|auto_v2|empty_v1|empty_v2)'.
* 'mount' means that different server instances will be using different
  Hashicorp Vault Server mounts. For instance, '/server1' for server1 and
 '/server2' for server2.
* 'directory' means that different server instances will be using different
  path suffixes (directories) under the same Hashicorp Vault Server mount.
  For instance, '/common/server_1' for server1 and '/common/server_2' for
  server2 where '/common' is a shared mount point.
* 'v1' means that all mount points being created will be of type 'kv'
  (version 1) and configuration files will have explicit
  'secret_mount_point_version = 1' parameter.
* 'v2' means that all mount points being created will be of type 'kv-v2'
  (version 2) and configuration files will have explicit
  'secret_mount_point_version = 2' parameter.
* 'auto_v1' means that all mount points being created will be of type 'kv'
  (version 1) and configuration files will have explicit
  'secret_mount_point_version = AUTO' parameter.
* 'auto_v2' means that all mount points being created will be of type 'kv-v2'
  (version 2) and configuration files will have explicit
  'secret_mount_point_version = AUTO' parameter.
* 'empty_v1' means that all mount points being created will be of type 'kv'
  (version 1) and configuration files will not have explicit
  'secret_mount_point_version' parameter.
* 'empty_v2' means that all mount points being created will be of type 'kv-v2'
  (version 2) and configuration files will not have explicit
  'secret_mount_point_version' parameter.

MTR combinations are created 

plugin/keyring_vault/vault_io.cc
@@ -1,16 +1,42 @@
+/* Copyright (c) 2018, 2021 Percona LLC and/or its affiliates. All rights
+   reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
 #include "vault_io.h"
-#include <curl/curl.h>
+
 #include <sstream>
+
+#include <boost/lexical_cast/try_lexical_convert.hpp>
+
+#include <curl/curl.h>
+
+#include "i_vault_parser_composer.h"
+#include "plugin/keyring/common/logger.h"
+#include "vault_credentials.h"
 #include "vault_credentials_parser.h"
+#include "vault_curl.h"
 #include "vault_keys_list.h"
 
 namespace keyring {
 
-bool Vault_io::init(std::string *keyring_storage_url) {
+bool Vault_io::init(const std::string *keyring_storage_url) {
   Vault_credentials_parser vault_credentials_parser(logger);
   Vault_credentials vault_credentials;
   return vault_credentials_parser.parse(*keyring_storage_url,
-                                        &vault_credentials) ||
+                                        vault_credentials) ||
          vault_curl->init(vault_credentials);
 }
 
@@ -59,11 +85,18 @@ bool Vault_io::get_serialized_object(ISerialized_object **serialized_object) {
   return false;
 }
 
+void Vault_io::set_curl_timeout(uint timeout) noexcept {
+  DBUG_ASSERT(vault_curl != nullptr);
+  vault_curl->set_timeout(timeout);
+}
+
 bool Vault_io::retrieve_key_type_and_data(IKey *key) {
   Secure_string json_response;
   if (vault_curl->read_key(static_cast<const Vault_key &>(*key),
                            &json_response) ||
-      vault_parser->parse_key_data(json_response, key)) {
+      vault_parser->parse_key_data(
+          json_response, key,
+          vault_curl->get_resolved_secret_mount_point_version())) {
     logger->log(MY_ERROR_LEVEL, ("Could not read key from Vault." +
                                  get_errors_from_response(json_response))
                                     .c_str());
@@ -104,7 +137,7 @@ bool Vault_io::flush_to_storage(ISerialized_object *serialized_object) {
   IKey *vault_key_raw = nullptr;
 
   if (serialized_object->get_next_key(&vault_key_raw) ||
-      vault_key_raw == NULL) {
+      vault_key_raw == nullptr) {
     delete vault_key_raw;
     return true;
   }

plugin/keyring_vault/vault_keys_container.cc
@@ -1,4 +1,23 @@
+/* Copyright (c) 2018, 2021 Percona LLC and/or its affiliates. All rights
+   reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
 #include "vault_keys_container.h"
+
+#include "i_vault_io.h"
 
 namespace keyring {
 bool Vault_keys_container::init(IKeyring_io *keyring_io_value,
@@ -22,6 +41,11 @@ IKey *Vault_keys_container::fetch_key(IKey *key) {
     return nullptr;
 
   return Keys_container::fetch_key(key);
+}
+
+void Vault_keys_container::set_curl_timeout(uint timeout) {
+  DBUG_ASSERT(vault_io != nullptr);
+  vault_io->set_curl_timeout(timeout);
 }
 
 bool Vault_keys_container::flush_to_backup() { return false; }

inikep/percona-xtrabackup

-------------------------------------------------------------------------------
PS-5364 feature: Update Hashicorp Vault plugin to use v2 API (Initial)
(cherry picked from commit 74987891fd33f7daedc50c76da6d013ebd66df9d)
(cherry picked from commit 364d3955ab78ae424444dda1e628cf75afbb2dee)
PS-5364 feature: Update Hashicorp Vault plugin to use v2 API (Major refactoring + gunit)
Changed the meaning of empty 'secret_mount_point_version' in the plugin
configuration file. It is now considered to be identical to 'AUTO'.
Currently, when this parameter is set to anything rather than '1', plugin
initialization routine needs to perform an additional listing request to the
Hashicorp Vault server ('LIST /v1/sys/mounts/'). In other words:
- if 'secret_mount_point_version' is '1', no 'LIST /v1/sys/mounts/' is
  performed and 'secret_mount_point' is always used as is to form key operation
  URLs
- if 'secret_mount_point_version' is '2', 'LIST /v1/sys/mounts/' is performed
  in order to split 'secret_mount_point' (say 'foo/bar/baz') into
  '<mount_point>' and '<directory>' components (either 'foo/bar' + 'baz' or
  'foo' + 'bar/baz') by iterating over registered mount points - this is
  necessary as in 'kv-v2' secrets URLs have additional 'data' / 'metadata'
  path parts in them ('foo/(meta)?data/bar/baz/...' or
  'foo/bar/(meta)?data/baz/...')
- if 'secret_mount_point_version' is either 'AUTO' or empty (parameter is not
  there at all), 'LIST /v1/sys/mounts/' also needs to be performed in order to
  detect the secret engine type('kv' or 'kv-v2') by looking into 'options'
  subobject of the mount point object corresponding to 'secret_mount_point'
  and checking whether it has 'version' ("1" or "2") in it
Please, notice that listing '/v1/sys/mounts/' requires additional privileges.
Added more validation rules to the plugin configuration parameters:
- 'vault_url' [required] - Vault Server Endpoint URL, must start with either
  'http://' of 'https://'
- 'secret_mount_point' [required] - 

plugin/keyring_vault/vault_io.cc
@@ -1,16 +1,42 @@
+/* Copyright (c) 2018, 2021 Percona LLC and/or its affiliates. All rights
+   reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
 #include "vault_io.h"
-#include <curl/curl.h>
+
 #include <sstream>
+
+#include <boost/lexical_cast/try_lexical_convert.hpp>
+
+#include <curl/curl.h>
+
+#include "i_vault_parser_composer.h"
+#include "plugin/keyring/common/logger.h"
+#include "vault_credentials.h"
 #include "vault_credentials_parser.h"
+#include "vault_curl.h"
 #include "vault_keys_list.h"
 
 namespace keyring {
 
-bool Vault_io::init(std::string *keyring_storage_url) {
+bool Vault_io::init(const std::string *keyring_storage_url) {
   Vault_credentials_parser vault_credentials_parser(logger);
   Vault_credentials vault_credentials;
   return vault_credentials_parser.parse(*keyring_storage_url,
-                                        &vault_credentials) ||
+                                        vault_credentials) ||
          vault_curl->init(vault_credentials);
 }
 
@@ -59,11 +85,18 @@ bool Vault_io::get_serialized_object(ISerialized_object **serialized_object) {
   return false;
 }
 
+void Vault_io::set_curl_timeout(uint timeout) noexcept {
+  assert(vault_curl != nullptr);
+  vault_curl->set_timeout(timeout);
+}
+
 bool Vault_io::retrieve_key_type_and_data(IKey *key) {
   Secure_string json_response;
   if (vault_curl->read_key(static_cast<const Vault_key &>(*key),
                            &json_response) ||
-      vault_parser->parse_key_data(json_response, key)) {
+      vault_parser->parse_key_data(
+          json_response, key,
+          vault_curl->get_resolved_secret_mount_point_version())) {
     logger->log(MY_ERROR_LEVEL, ("Could not read key from Vault." +
                                  get_errors_from_response(json_response))
                                     .c_str());
@@ -104,7 +137,7 @@ bool Vault_io::flush_to_storage(ISerialized_object *serialized_object) {
   IKey *vault_key_raw = nullptr;
 
   if (serialized_object->get_next_key(&vault_key_raw) ||
-      vault_key_raw == NULL) {
+      vault_key_raw == nullptr) {
     delete vault_key_raw;
     return true;
   }

plugin/keyring_vault/vault_keys_container.cc
@@ -1,4 +1,23 @@
+/* Copyright (c) 2018, 2021 Percona LLC and/or its affiliates. All rights
+   reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
 #include "vault_keys_container.h"
+
+#include "i_vault_io.h"
 
 namespace keyring {
 bool Vault_keys_container::init(IKeyring_io *keyring_io_value,
@@ -22,6 +41,11 @@ IKey *Vault_keys_container::fetch_key(IKey *key) {
     return nullptr;
 
   return Keys_container::fetch_key(key);
+}
+
+void Vault_keys_container::set_curl_timeout(uint timeout) {
+  assert(vault_io != nullptr);
+  vault_io->set_curl_timeout(timeout);
 }
 
 bool Vault_keys_container::flush_to_backup() { return false; }